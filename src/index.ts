ff2d5640f72f117086633a317e235ebfa2b200ff
Andrew Miller <andrew@westlakelabs.com>
2024-05-03 13:26:59 -0700

    Re-export elements from critical dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+export * from '@hyperproof/hypersync-models';
+
+export {
+  CredentialFieldType,
+  CustomAuthCredentials,
+  Logger
+} from '@hyperproof/integration-sdk';
+
 export * from './hypersync';
